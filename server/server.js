2fd438454f8570e949f34bba3b2ae3527f38d838
git <kieronmcc@gmail.com>
2018-12-12 11:40:46 +0100

    Added  basic socket IO connection

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -9,10 +11,31 @@ const port = process.env.PORT || 3000;
 
 // Grab handle to express webappframework
 var app = express();
+// var server = http.createServer((req, res) => {
+//
+// });
+// Equivalent to code above because express
+// is built on node http
+var server = http.createServer(app);
+//make our server a web socket event handler
+var io = socketIO(server);
+
 
 // Setup static page middleware to serve public folder
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+// Register an event listener on socket io object
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client has disconnected');
+  });
+});
+
+// so we use the http server instead of app to listen
+// So all we used express for here was to setup some
+// callbacks and listeners for the http object (server)
+server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
