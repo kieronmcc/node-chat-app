98f9b747948073991905e58f5182fedb7201187a
git <kieronmcc@gmail.com>
2018-12-13 14:41:04 +0100

    Add geolocation support via geolocation API

--- public/js/index.js
+++ public/js/index.js
@@ -17,6 +17,20 @@ socket.on('newMessage', function (msg) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function (message) {
+  var li = jQuery('<li></li>'); //Render list
+  //render anchor tag 'target=_blank' open link in new tab
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  // safer this way than via template string as stops malicious User
+  // from injecting html
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+
+  li.append(a); // url anchor to list
+  jQuery('#messages').append(li); // add updated list to HTML DOM
+});
+
 jQuery('#message-form').on('submit', function (event) {
   event.preventDefault();
 
@@ -25,5 +39,23 @@ jQuery('#message-form').on('submit', function (event) {
     text: jQuery('[name=message]').val()
   }, function () {
 
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function () {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser');
+  }
+
+  //From browser library - geolocation support
+  //See https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API
+  navigator.geolocation.getCurrentPosition(function (position) {
+    socket.emit('createLocationMessage', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function () {
+    alert('Unable to fetch location.');
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,14 +1,33 @@
 var expect = require('expect');
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 // Testsuite for generateMessage function
-describe('', () => {
+describe('generateMessage Testsuite', () => {
   it('should generate the correct message object', () => {
     var from = 'Test User';
     var text = 'Test text';
+
+    // Call function under test
     var msg = generateMessage( from, text);
 
+    // Assert reponse values
     expect(msg).toInclude({from, text});
+    expect(typeof msg.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMessage Testsuite', () => {
+  it('should generate correct location object', () => {
+    var from = 'Test User';
+    var lat = 123;
+    var lng = 345;
+    var url = `https://www.google.com/maps?q=${lat},${lng}`;
+
+    // Call function under test
+    var msg = generateLocationMessage(from, lat, lng);
+
+    // Assert reponse values
+    expect(msg).toInclude({from, url});
     expect(typeof msg.createdAt).toBe('number');
   });
 });
